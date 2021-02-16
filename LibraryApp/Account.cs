6dec9fb179676dd6c9f232467296f214703bca7d
Tapasya Khatri <takhat014@gmail.com>
2021-02-16 13:59:32 -0800

    Add project files.

---
+++ LibraryApp/Account.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryApp
+{
+    /// <summary>
+    /// This represents a library account where you can access library books
+    /// </summary>
+    public class Account
+    {
+        # region Properties
+        public int AccountNumber { get; set; }
+        public string EmailAddress { get; set; }
+        public DateTime CreatedDate { get; set; }
+        #endregion
+    }
+}
---
+++ LibraryApp/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryApp
+{
+    public class Book
+    {
+        public int ISBN { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Edition { get; set; }
+        public int NumberOfCopiesTotal { get; set; }
+        public int NumberOfCopiesAvailableForHold { get; set; }
+    }
+}
